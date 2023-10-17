3dccfcf366959f859d4ecbef198ab40fb12dc448
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-10-18 02:15:27 +0300

    Fix some issues

--- app/src/main/java/com/example/movieproject/Activity/IntroActivity.java
+++ app/src/main/java/com/example/movieproject/Activity/IntroActivity.java
@@ -1,16 +1,28 @@
 package com.example.movieproject.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.movieproject.R;
 
 public class IntroActivity extends AppCompatActivity {
-
+    private static final int SPLASH_TIME_OUT = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // close the splash activity
+            }
+        }, SPLASH_TIME_OUT);
+
     }
 }
