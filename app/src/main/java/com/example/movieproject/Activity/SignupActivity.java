14ddbf6d304ef6c6e3df530448fe4356690d071a
Ahmed M Hamza <71948774+ahmed-coding@users.noreply.github.com>
2023-10-19 22:33:15 +0300

    Complete The project and start In Edite and make it butter.

---
+++ app/src/main/java/com/example/movieproject/Activity/SignupActivity.java
@@ -0,0 +1,18 @@
+package com.example.movieproject.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.movieproject.R;
+
+public class SignupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+
+    }
+}
